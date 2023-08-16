f25727bb5175f608ad8481a4c3f01867c19c1bae
Jason Long <jal@gatech.edu>
2023-08-16 14:52:50 -0500

    finish exercise 1.6 and 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -7,25 +7,48 @@ const App = () => {
   const [bad, setBad] = useState(0);
 
   const handleGood = () => {
-    return;
+    setGood(good + 1);
+  };
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const handleBad = () => {
+    setBad(bad + 1);
+  };
+
+  const sumOfRatings = () => {
+    return good + neutral + bad;
+  };
+
+  const getAveragePercent = () => {
+    let value = (good - bad) / sumOfRatings();
+    return isNaN(value) ? "" : value.toFixed(2).toString();
+  };
+
+  const getPositivePercent = () => {
+    let value = good / sumOfRatings();
+    // truncate to two decimal places
+    return isNaN(value) ? "" : value.toFixed(2).toString().concat("%");
   };
 
   return (
     <div>
       <div>
-        <p>give feedback</p>
+        <h1>give feedback</h1>
         <Button handleClick={handleGood} text="good" />
-        <Button text="neutral" />
-        <Button text="bad" />
+        <Button handleClick={handleNeutral} text="neutral" />
+        <Button handleClick={handleBad} text="bad" />
       </div>
       <div>
-        <p>statistics:</p>
-        <p>good:</p>
-        <p>neutral:</p>
-        <p>bad:</p>
-        <p>all:</p>
-        <p>average:</p>
-        <p>positive:</p>
+        <h2>statistics</h2>
+        <p>good: {good}</p>
+        <p>neutral: {neutral}</p>
+        <p>bad: {bad}</p>
+        <p>all: {sumOfRatings()}</p>
+        <p>average: {getAveragePercent()}</p>
+        <p>positive: {getPositivePercent()}</p>
       </div>
     </div>
   );
