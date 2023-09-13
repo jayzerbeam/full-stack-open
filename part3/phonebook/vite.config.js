8821a1f56c4578c09120535586c5c732b2277db6
Jason Long <jal@gatech.edu>
2023-09-12 22:07:15 -0500

    update vite proxy and push test data to the mongo db

--- part3/phonebook/vite.config.js
+++ part3/phonebook/vite.config.js
@@ -6,8 +6,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:3001",
-      changeOrigin: true,
+      "/api": {
+        target: "http://localhost:3001",
+        changeOrigin: true,
+      },
     },
   },
 });
