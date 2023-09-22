f4905146f8788d3e26e0d85f0a29156ac524bb52
Jason Long <jal@gatech.edu>
2023-09-21 22:57:40 -0500

    finish exercise 3.13

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -22,89 +22,96 @@ app.use(
 app.use(cors());
 app.use(express.static("dist"));
 
-const getEntry = (id) => {
-  return phonebook.find((item) => {
-    return item.id === id;
-  });
-};
-
-const removeEntry = (id) => {
-  return phonebook.filter((item) => {
-    return item.id !== id;
-  });
-};
-
-const makeRandomNumber = () => {
-  const max = 999_999_999;
-  return Math.floor(Math.random() * max);
-};
-
-const incrementId = () => {
-  const maxId =
-    phonebook.length > 0 ? Math.max(...phonebook.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
-
-const hasDupeName = (name) => {
-  return phonebook.find(
-    (entry) => entry.name.toLowerCase() === name.toLowerCase(),
-  );
-};
+/*
+ * GET requests
+ * */
 
 app.get("/api/persons", (_, res) => {
-  res.json(phonebook);
-});
-
-app.get("/info", (_, res) => {
-  res.send(
-    `<p>phonebook has info for ${
-      phonebook.length
-    } people</p><p>request info: ${new Date()}</p>`,
-  );
-});
-
-app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const entry = getEntry(id);
-
-  if (entry) {
+  Entry.find({}).then((entry) => {
+    console.log(entry);
     res.json(entry);
-  } else {
-    res.status(404).end();
-  }
+  });
 });
 
-app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  phonebook = removeEntry(id);
-  console.log(phonebook);
-
-  res.status(204).end();
-});
-
-app.post("/api/persons/", (req, res) => {
-  const body = req.body;
-
-  const entry = {
-    id: incrementId(),
-    name: body.name,
-    number: String(makeRandomNumber()),
-  };
-
-  if (!entry.name) {
-    return res.status(400).json({
-      error: "name is missing",
-    });
-  } else if (hasDupeName(entry.name)) {
-    return res.status(400).json({
-      error: "entry for this name already exists",
-    });
-  }
-
-  phonebook.concat(entry);
-
-  res.json(entry);
-});
+// app.get("/info", (_, res) => {
+//   res.send(
+//     `<p>phonebook has info for ${
+//       phonebook.length
+//     } people</p><p>request info: ${new Date()}</p>`,
+//   );
+// });
+//
+// app.get("/api/persons/:id", (req, res) => {
+//   const id = Number(req.params.id);
+//   const entry = getEntry(id);
+//
+//   if (entry) {
+//     res.json(entry);
+//   } else {
+//     res.status(404).end();
+//   }
+// });
+//
+// const getEntry = (id) => {
+//   return phonebook.find((item) => {
+//     return item.id === id;
+//   });
+// };
+//
+// const removeEntry = (id) => {
+//   return phonebook.filter((item) => {
+//     return item.id !== id;
+//   });
+// };
+//
+// const makeRandomNumber = () => {
+//   const max = 999_999_999;
+//   return Math.floor(Math.random() * max);
+// };
+//
+// const incrementId = () => {
+//   const maxId =
+//     phonebook.length > 0 ? Math.max(...phonebook.map((n) => n.id)) : 0;
+//   return maxId + 1;
+// };
+//
+// const hasDupeName = (name) => {
+//   return phonebook.find(
+//     (entry) => entry.name.toLowerCase() === name.toLowerCase(),
+//   );
+// };
+//
+// app.delete("/api/persons/:id", (req, res) => {
+//   const id = Number(req.params.id);
+//   phonebook = removeEntry(id);
+//   console.log(phonebook);
+//
+//   res.status(204).end();
+// });
+//
+// app.post("/api/persons/", (req, res) => {
+//   const body = req.body;
+//
+//   const entry = {
+//     id: incrementId(),
+//     name: body.name,
+//     number: String(makeRandomNumber()),
+//   };
+//
+//   if (!entry.name) {
+//     return res.status(400).json({
+//       error: "name is missing",
+//     });
+//   } else if (hasDupeName(entry.name)) {
+//     return res.status(400).json({
+//       error: "entry for this name already exists",
+//     });
+//   }
+//
+//   phonebook.concat(entry);
+//
+//   res.json(entry);
+// });
 
 app.listen(PORT, () => {
   console.log(`listening on PORT ${PORT}`);
